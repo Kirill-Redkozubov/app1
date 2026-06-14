1aa029373395af8d9a54b30eb7f305342e7271a0
Kirill Redkozubov <kirillred1996@mail.ru>
2026-06-13 19:51:23 -0700

    подключили папки static и templates и настроили оформление страницы через load static

--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    context = {
+        "title": "Home",
+        "content": "Главная страница магазина - Home",
+    }
+    return render(request, "main/index.html", context)
+
+
+def about(request):
+    return HttpResponse("About page")
