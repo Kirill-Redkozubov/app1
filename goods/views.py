57216ed642895beb85e8c716a8b3c29116eeb562
Kirill Redkozubov <kirillred1996@mail.ru>
2026-06-15 16:57:27 -0700

    Добавили пагинацию

--- goods/views.py
+++ goods/views.py
@@ -1,16 +1,23 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_list_or_404
 from goods.models import Products
+from django.core.paginator import Paginator
 # Create your views here.
 
 def catalog(request, category_slug):
+    page = request.GET.get("page", 1)
+
     if category_slug == "all":
         goods = Products.objects.all()
     else:
-        goods = get_object_or_404(Products.objects.filter(category__slug=category_slug))
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
         
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(int(page))
+    
     context = {
         "title": "Home - Каталог",
-        "goods": goods,
+        "goods": current_page,
+        "slug_url": category_slug,
     }
     return render(request, "goods/catalog.html", context)
 
